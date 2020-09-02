c8999c108a8c66735b07d0d32b33168dd54eeadd
April Adams <illusionlace@gmail.com>
2020-09-02 02:52:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,29 +1,29 @@
 def sort_array_asc(arr)
   arr.sort
-end 
+end
 def sort_array_desc(arr)
   arr.sort.reverse
 end
 def sort_array_char_count(arr)
   arr.sort { |a, b| a.length <=> b.length}
-end 
+end
 def swap_elements(arr)
   arr[1], arr[2] = arr[2], arr[1]
-arr 
-end 
+arr
+end
 def reverse_array(arr)
   arr.reverse
 end 
 def kesha_maker(arr)
   arr.each {|x| x[2] = "$"}
-end 
+end
 def find_a(arr)
   arr.select {|x| x.start_with?("a")}
-end 
+end
 def sum_array(arr)
   arr.reduce(:*)
-end 
+end
 def add_s(arr)
   arr.map.with_index do |element, index|
     index != 1 ? element << "s" : element
-  end 
+  end
